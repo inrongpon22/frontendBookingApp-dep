67854c0e49520020b6c76f97f0bf6d742cd0502d
Tastai44 <74952316+Tastai44@users.noreply.github.com>
2024-05-17 16:58:31 +0700

    chore: Update localization and add manual booking feature, multi select time reservation

--- src/App.tsx
+++ src/App.tsx
@@ -21,85 +21,92 @@ import DayOffSetting from "./pages/dayoff-setting/DayOffSetting.tsx";
 import AddNewDayOff from "./pages/dayoff-setting/AddNewDayOff.tsx";
 
 function App() {
-  useEffect(() => {
-    localStorage.setItem("lang", "th");
-  }, []);
+    useEffect(() => {
+        localStorage.setItem("lang", "th");
+    }, []);
 
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          {/* customer */}
-          <Route
-            path="/details/:businessId"
-            element={<ShopDetailsPageWrapper />}
-          />
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    {/* customer */}
+                    <Route
+                        path="/details/:businessId"
+                        element={<ShopDetailsPageWrapper />}
+                    />
 
-          <Route path="/booking-success" element={<BookingSummaryWrapper />} />
-          <Route path="/my-bookings" element={<MyBookingWrapper />} />
-          <Route
-            path="/booking/:bookingId"
-            element={<BookingSummaryWrapper />}
-          />
+                    <Route
+                        path="/booking-success"
+                        element={<BookingSummaryWrapper />}
+                    />
+                    <Route path="/my-bookings" element={<MyBookingWrapper />} />
+                    <Route
+                        path="/booking/:bookingId"
+                        element={<BookingSummaryWrapper />}
+                    />
 
+                    {/* business */}
+                    <Route path="/" element={<BusinessAuth />} />
+                    <Route
+                        path="/business-profile/:businessId"
+                        element={<BusinessProfile />}
+                    />
+                    <Route
+                        path="/booking-approval/:businessId/:serviceId"
+                        element={<BookingApproval />}
+                    />
+                    <Route
+                        path="/business-overview"
+                        element={<BusinessOverview />}
+                    />
+                    <Route
+                        path="/create-business"
+                        element={<CreateBusiness />}
+                    />
+                    <Route
+                        path="/business-setting/:businessId"
+                        element={<BusinessSetting />}
+                    />
 
-
-          {/* business */}
-          <Route path="/" element={<BusinessAuth />} />
-          <Route
-            path="/business-profile/:businessId"
-            element={<BusinessProfile />}
-          />
-          <Route
-            path="/booking-approval/:businessId/:serviceId"
-            element={<BookingApproval />}
-          />
-          <Route path="/business-overview" element={<BusinessOverview />} />
-          <Route path="/create-business" element={<CreateBusiness />} />
-          <Route
-            path="/business-setting/:businessId"
-            element={<BusinessSetting />}
-          />
-
-
-          {/* service */}
-          <Route
-            path="/service-info/:businessId"
-            element={<ServiceInfo isClose={false} isEdit={false} />}
-          />
-          <Route path="/service-time/:businessId" element={<ServiceTime />} />
-          {/* <Route
+                    {/* service */}
+                    <Route
+                        path="/service-info/:businessId"
+                        element={<ServiceInfo isClose={false} isEdit={false} />}
+                    />
+                    <Route
+                        path="/service-time/:businessId"
+                        element={<ServiceTime />}
+                    />
+                    {/* <Route
             path="/create-service/:businessId"
             element={<CreateService />}
           /> */}
-          <Route
-            path="/service-setting/:businessId"
-            element={<ServiceSetting />}
-          />
-          <Route
-            path="/service-detail/:businessId/:serviceId"
-            element={<ServiceDetail serviceId={0} />}
-          />
-          <Route
-            path="/service-detail/:businessId/:serviceId"
-            element={<ServiceDetail serviceId={0} />}
-          />
+                    <Route
+                        path="/service-setting/:businessId"
+                        element={<ServiceSetting />}
+                    />
+                    <Route
+                        path="/service-detail/:businessId/:serviceId"
+                        element={<ServiceDetail serviceId={0} />}
+                    />
+                    <Route
+                        path="/service-detail/:businessId/:serviceId"
+                        element={<ServiceDetail serviceId={0} />}
+                    />
 
-
-
-          {/* day off */}
-          <Route
-            path="/dayoff-setting/:businessId"
-            element={<DayOffSetting />}
-          />
-          <Route
-            path="/dayoff-setting/:businessId/add-new"
-            element={<AddNewDayOff />}
-          />
-        </Routes>
-      </BrowserRouter>
-      <Toaster />
-    </>
-  );
+                    {/* day off */}
+                    <Route
+                        path="/dayoff-setting/:businessId"
+                        element={<DayOffSetting />}
+                    />
+                    <Route
+                        path="/dayoff-setting/:businessId/add-new"
+                        element={<AddNewDayOff />}
+                    />
+                </Routes>
+            </BrowserRouter>
+            <Toaster />
+        </>
+    );
 }
 export default App;
--- src/pages/auth/BusinessAuth.tsx
+++ src/pages/auth/BusinessAuth.tsx
@@ -4,51 +4,71 @@ import DialogWrapper from "../../components/dialog/DialogWrapper";
 // api
 import { getBusinessByUserId } from "../../api/business";
 import { useNavigate } from "react-router";
+// import logo from "../../../public/images/logo.png";
 
 const BusinessAuth = () => {
-  const navigate = useNavigate();
-  const { t } = useTranslation();
+    const navigate = useNavigate();
+    const { t } = useTranslation();
 
-  const token = localStorage.getItem("token") ?? "";
-  const userId = localStorage.getItem("userId") ?? "";
+    const token = localStorage.getItem("token") ?? "";
+    const userId = localStorage.getItem("userId") ?? "";
 
-  const [showDialog, setShowDialog] = useState<boolean>(false);
-  const [dialogState, setDialogState] = useState<string | undefined>(
-    "phone-input"
-  );
+    const [showDialog, setShowDialog] = useState<boolean>(false);
+    const [dialogState, setDialogState] = useState<string | undefined>(
+        "phone-input"
+    );
 
-  useEffect(() => {
-    document.title = t("title:bussRootTitle");
-    if (token) {
-      getBusinessByUserId(userId, token).then((res) =>
-      navigate(`/business-profile/${res[0].id}`)
-      );
-    }
-  }, []);
+    useEffect(() => {
+        document.title = t("title:bussRootTitle");
+        if (token) {
+            getBusinessByUserId(userId, token).then((res) =>
+                navigate(`/business-profile/${res[0].id}`)
+            );
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  return (
-    <div className="flex flex-col justify-center items-center h-dvh">
-      <div className="w-4/6 flex flex-col gap-4 justify-center items-center">
-        <p className="text-[25px] font-semibold">{t("title:bussRootTitle")}</p>
-        <p className="text-center">{t("desc:bussRootDesc")}</p>
-        <button
-          type="button"
-          className="w-2/3 py-3 px-10 bg-[#020873] text-white rounded-lg"
-          onClick={() => setShowDialog(true)}
-        >
-          {t("button:getStartedButton")}
-        </button>
-      </div>
+    return (
+        <div className="flex flex-col justify-center items-center h-dvh">
+            <div className="h-screen w-5/6 flex flex-col justify-between items-center">
+                <div className="flex flex-col">
+                    <div className="flex justify-center my-12">
+                        <img
+                            src={"./smallLogo.svg"}
+                            alt="logo"
+                            style={{
+                                width: "40vw",
+                            }}
+                        />
+                    </div>
+                    <div className="flex flex-col gap-4 items-center">
+                        <div className="text-[32px] font-bold text-center ">
+                            {t("title:conceptWord")}
+                        </div>
+                        <div className="text-center">
+                            {t("desc:desConceptWord")}
+                        </div>
+                    </div>
+                </div>
 
-      <DialogWrapper
-        show={showDialog}
-        setShow={setShowDialog}
-        userSide="business"
-        dialogState={dialogState}
-        setDialogState={setDialogState}
-      />
-    </div>
-  );
+                <button
+                    type="button"
+                    style={{ marginBottom: "56px" }}
+                    className="py-3 px-10 bg-[#020873] text-white rounded-lg"
+                    onClick={() => setShowDialog(true)}>
+                    {t("button:getStartedButton")}
+                </button>
+            </div>
+
+            <DialogWrapper
+                show={showDialog}
+                setShow={setShowDialog}
+                userSide="business"
+                dialogState={dialogState}
+                setDialogState={setDialogState}
+            />
+        </div>
+    );
 };
 
 export default BusinessAuth;
