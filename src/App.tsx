5fd94144de360bb508a29bbb37ad76f0b6e5e1ea
Tastai44 <tastai_kh@cmu.ac.th>
2024-06-02 09:49:40 +0700

    refactor: Increase callback timeout to 5 seconds and fix login bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-/** @format */
-
 import { Toaster } from "react-hot-toast";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import "./i18n.ts";
 // components
 import ShopDetailsPageWrapper from "./pages/shop-detials/ShopDetailsPageWrapper";
@@ -22,78 +20,72 @@ import Unauthorized from "./pages/errors/401Unauthorized.tsx";
 import Forbidden from "./pages/errors/403Forbidden.tsx";
 import CallBack from "./pages/auth/CallBack.tsx";
 import Noti from "./pages/notification/Noti.tsx";
-import { useAuth } from "./contexts/AuthContext.tsx";
-// import ProtectedRoute from "./pages/auth/ProtectedRoute.tsx";
+import { useEffect, useState } from "react";
+import { app_api } from "./helper/url.ts";
+import axios from "axios";
+import Loading from "./components/dialog/Loading.tsx";
 
 function App() {
-    const { isAuthenticated } = useAuth();
-    console.log(isAuthenticated);
+    const [isLoadding, setIsLoading] = useState<boolean>(false);
+
+    function ProtectedRoute() {
+        const navigate = useNavigate();
+
+        useEffect(() => {
+            setIsLoading(true);
+            const token = localStorage.getItem("token");
+            const checkTokenValidity = async () => {
+                try {
+                    const response = await axios.get(`${app_api}/auth/status`, {
+                        headers: {
+                            Authorization: token,
+                        },
+                    });
+                    return response;
+                } catch (error) {
+                    console.error("Token validation error:", error);
+                    navigate("/"); // Redirect to the login page
+                    setIsLoading(false);
+                }
+            };
+
+            if (token) {
+                checkTokenValidity();
+                setIsLoading(false);
+            } else {
+                console.log("Token is not valid");
+                navigate('/');
+                setIsLoading(false);
+            }
+
+        }, [navigate]);
+
+        return (
+            <Routes>
+                <Route path="/business-profile/:businessId" element={<BusinessProfile />} />
+                <Route path="/booking-approval/:businessId" element={<BookingApproval />} />
+                <Route path="/my-bookings" element={<MyBookingWrapper />} />
+                <Route path="/business-overview" element={<BusinessOverview />} />
+                <Route path="/create-business" element={<BusinessSetting />} />
+                <Route path="/business-setting" element={<BusinessSetting />} />
+                <Route path="/service/:businessId" element={<ServiceDetail serviceId={0} />} />
+                <Route path="/service-setting/:businessId" element={<ServiceSetting />} />
+                <Route path="/service-detail/:businessId/:serviceId" element={<ServiceDetail serviceId={0} />} />
+                <Route path="/noti/:businessId" element={<Noti />} />
+                <Route path="/dayoff-setting/:businessId" element={<DayOffSetting />} />
+                <Route path="/dayoff-setting/:businessId/add-new" element={<AddNewDayOff />} />
+                <Route path="*" element={<Navigate to="/404" replace />} />
+            </Routes>
+        );
+    }
 
     return (
         <>
+            <Loading openLoading={isLoadding} />
             <BrowserRouter>
                 <Routes>
-                    {/* root */}
                     <Route path="/" element={<BusinessAuth />} />
-
-                    {/* protected route */}
-                    {/* <Route element={<ProtectedRoute />}> */}
-                    {/* protected business routes */}
-                    <Route
-                        path="/business-profile/:businessId"
-                        element={<BusinessProfile />}
-                    />
-                    <Route
-                        path="/booking-approval/:businessId"
-                        element={<BookingApproval />}
-                    />
-                    <Route path="/my-bookings" element={<MyBookingWrapper />} />
-                    <Route
-                        path="/booking/:bookingId"
-                        element={<BookingSummaryWrapper />}
-                    />
-                    <Route
-                        path="/business-overview"
-                        element={<BusinessOverview />}
-                    />
-                    <Route
-                        path="/create-business"
-                        element={<BusinessSetting />}
-                    />
-                    <Route
-                        path="/business-setting"
-                        element={<BusinessSetting />}
-                    />
-
-                    {/* service */}
-                    <Route
-                        path="/service/:businessId"
-                        element={<ServiceDetail serviceId={0} />}
-                    />
-                    <Route
-                        path="/service-setting/:businessId"
-                        element={<ServiceSetting />}
-                    />
-                    <Route
-                        path="/service-detail/:businessId/:serviceId"
-                        element={<ServiceDetail serviceId={0} />}
-                    />
-                    <Route path="/noti/:businessId" element={<Noti />} />
-
-                    {/* day off */}
-                    <Route
-                        path="/dayoff-setting/:businessId"
-                        element={<DayOffSetting />}
-                    />
-                    <Route
-                        path="/dayoff-setting/:businessId/add-new"
-                        element={<AddNewDayOff />}
-                    />
-                    {/* day off */}
-                    {/* </Route> */}
-
-                    {/* customer */}
-
+                    <Route path="/line-login" element={<CallBack />} />
                     <Route
                         path="/details/:businessId"
                         element={<ShopDetailsPageWrapper />}
@@ -102,23 +94,15 @@ function App() {
                         path="/booking-success"
                         element={<BookingSummaryWrapper />}
                     />
-
-                    {/* 404 not found */}
-                    <Route path="*" element={<Navigate to="/404" />} />
-                    <Route path="/404" element={<NotFound />} />
-
-                    {/* login from line */}
-                    <Route path="/line-login" element={<CallBack />} />
-                    {/* login from line */}
-
-                    {/* 401 unauthorize */}
                     <Route path="/401" element={<Unauthorized />} />
-                    {/* 401 unauthorize */}
                     <Route path="/403" element={<Forbidden />} />
+                    <Route path="/404" element={<NotFound />} />
+                    <Route path="*" element={<ProtectedRoute />} />
                 </Routes>
             </BrowserRouter>
             <Toaster />
         </>
     );
 }
+
 export default App;
--- src/pages/auth/CallBack.tsx
+++ src/pages/auth/CallBack.tsx
@@ -39,14 +39,14 @@ export default function CallBack() {
                             setIsLoading(false);
                             navigate(`/business-profile/${resp.data[0].id}`);
                         }
-                    }, 3000);
+                    }, 5000);
                 })
                 .catch((err) => {
                     if (err.response.status === 404) {
                         setTimeout(() => {
                             setIsLoading(false);
                             navigate("/create-business");
-                        }, 3000);
+                        }, 5000);
                     } else {
                         console.log(err.message);
                         toast.error("มีบางอย่างผิดพลาด กรุณาลองใหม่อีกครั้ง");
