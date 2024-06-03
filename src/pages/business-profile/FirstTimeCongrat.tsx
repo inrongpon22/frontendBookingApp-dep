e4a0799d3a87f052b29fc131162362b44f84414c
Tastai44 <tastai_kh@cmu.ac.th>
2024-06-03 18:12:45 +0700

    refactor: Add QR code sharing functionality to FirstTimeCongrat component

--- src/pages/business-profile/FirstTimeCongrat.tsx
+++ src/pages/business-profile/FirstTimeCongrat.tsx
@@ -1,35 +1,47 @@
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import { useParams } from "react-router-dom";
 import { shareBookingLink } from "../../helper/alerts";
+import ShareQR from "../../components/dialog/ShareQR";
+import { t } from "i18next";
+import { useState } from "react";
 
 interface FirstTimeCongratProps {
-    handleClose:Function
+    handleClose: Function;
 }
 
-const FirstTimeCongrat = ({handleClose}:FirstTimeCongratProps) => {
+const FirstTimeCongrat = ({ handleClose }: FirstTimeCongratProps) => {
     const { businessId } = useParams();
+    const [openShareQR, setOpenShareQR] = useState<boolean>(false);
     return (
-        <div className="bg-[#2E7CF6] bg-opacity-10 p-5 mb-2">
-            <p className="flex justify-between items-center">
-                <span className="text-[17px] font-bold">ยินดีด้วย! 🎉</span>
-                <CloseRoundedIcon
-                    fontSize="small"
-                    color="disabled"
-                    onClick={() => handleClose()}
-                />
-            </p>
-            <p className="py-2">
-                คุณได้เปิดร้านของคุณเรียบร้อยแล้ว!
-                แชร์ลิ้งการจองร้านของคุณเพื่อต้อนรับลูกค้า.
-            </p>
-            <button
-                type="button"
-                className="w-full bg-deep-blue bg-opacity-80 p-3 mt-3 text-[14px] font-semibold text-white rounded-lg"
-                onClick={() => shareBookingLink(businessId)}
-            >
-                แชร์ลิ้งการจองร้านของคุณ
-            </button>
-        </div>
+        <>
+            <ShareQR
+                open={openShareQR}
+                url={`${window.location.origin}/details/${businessId}`}
+                businessId={businessId ?? ""}
+                handleClose={() => setOpenShareQR(false)} />
+            <div className="bg-[#2E7CF6] bg-opacity-10 p-5 mb-2">
+                <p className="flex justify-between items-center">
+                    <span className="text-[17px] font-bold">ยินดีด้วย! 🎉</span>
+                    <CloseRoundedIcon
+                        fontSize="small"
+                        color="disabled"
+                        onClick={() => handleClose()}
+                    />
+                </p>
+                <p className="py-2">
+                    คุณได้เปิดร้านของคุณเรียบร้อยแล้ว!
+                    แชร์ลิ้งการจองร้านของคุณเพื่อต้อนรับลูกค้า.
+                </p>
+                <button
+                    type="button"
+                    className="w-full bg-deep-blue bg-opacity-80 p-3 mt-3 text-[14px] font-semibold text-white rounded-lg"
+                    onClick={() => setOpenShareQR(true)}
+                >
+                    {t("button:shareBookingWeb")}
+                </button>
+            </div>
+        </>
+
     );
 };
 
